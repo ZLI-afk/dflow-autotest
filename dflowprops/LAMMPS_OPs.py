f728f2fc9f3ed8bbfa8300d6b862e46020e55165
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2023-02-07 12:56:15 +0800

    add first commit

---
+++ dflowprops/LAMMPS_OPs.py
@@ -0,0 +1,205 @@
+from typing import List
+from dflow import (
+    Workflow,
+    Step,
+    argo_range,
+    SlurmRemoteExecutor,
+    upload_artifact,
+    download_artifact,
+    InputArtifact,
+    OutputArtifact,
+    ShellOPTemplate
+)
+from dflow.python import (
+    PythonOPTemplate,
+    OP,
+    OPIO,
+    OPIOSign,
+    Artifact,
+    Slices,
+    upload_packages
+)
+import time
+
+import subprocess, os, shutil, glob, dpdata, pathlib
+from pathlib import Path
+from typing import List
+from dflow.plugins.bohrium import BohriumContext, BohriumExecutor
+from dpdata.periodic_table import Element
+from monty.serialization import loadfn
+
+try:
+    from dpgen.auto_test.common_equi import (make_equi, post_equi)
+except:
+    pass
+
+from dflow.python import upload_packages
+
+
+import shutil
+upload_packages.append(__file__)
+
+
+def return_prop_list(parameters):
+    prop_list = []
+    for ii in parameters:
+        if ii.get('skip', False):
+            continue
+        if 'init_from_suffix' and 'output_suffix' in ii:
+            #do_refine = True
+            suffix = ii['output_suffix']
+        elif 'reproduce' in ii and ii['reproduce']:
+            #do_refine = False
+            suffix = 'reprod'
+        else:
+            #do_refine = False
+            suffix = '00'
+        prop_list.append(ii['type'] + '_' + suffix)
+    return prop_list
+
+
+class PropsMakeLAMMPS(OP):
+    """
+    class for making calculation tasks
+    """
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign({
+            'input': Artifact(Path)
+        })
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign({
+            'output': Artifact(Path),
+            'njobs': int,
+            'jobs': Artifact(List[Path])
+        })
+
+    @OP.exec_sign_check
+    def execute(
+            self,
+            op_in: OPIO,
+    ) -> OPIO:
+        cwd = os.getcwd()
+
+        os.chdir(op_in["input"])
+        work_d = os.getcwd()
+        param_argv = 'param_prop.json'
+        structures = loadfn(param_argv)["structures"]
+        inter_parameter = loadfn(param_argv)["interaction"]
+        parameter = loadfn(param_argv)["properties"]
+        cmd = f'dpgen autotest make {param_argv}'
+        subprocess.call(cmd, shell=True)
+
+        conf_dirs = []
+        for conf in structures:
+            conf_dirs.extend(glob.glob(conf))
+        conf_dirs.sort()
+
+        prop_list = return_prop_list(parameter)
+        task_list = []
+        for ii in conf_dirs:
+            #relaxation = os.path.join(ii, 'relaxation')
+            #relax_task = os.path.join(relaxation, 'relax_task')
+            conf_dir_global = os.path.join(work_d, ii)
+            for jj in prop_list:
+                task_list.append(os.path.join(conf_dir_global, jj))
+            """
+            for jj in prop_list:
+                prop = os.path.join(conf_dir_global, jj)
+                os.chdir(prop)
+                prop_tasks = glob.glob(os.path.join(prop, 'task.*'))
+                prop_tasks.sort()
+                for kk in prop_tasks:
+                    #bbb = kk
+                    task_list.append(kk)
+            """
+
+        all_jobs = task_list
+        njobs = len(all_jobs)
+        jobs = []
+        for job in all_jobs:
+            jobs.append(pathlib.Path(job))
+
+        os.chdir(cwd)
+        op_out = OPIO({
+            "output": op_in["input"],
+            "njobs": njobs,
+            "jobs": jobs
+        })
+        return op_out
+
+
+class LAMMPS(OP):
+    """
+    class for LAMMPS calculation
+    """
+
+    def __init__(self, infomode=1):
+        self.infomode = infomode
+
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign({
+            'input_lammps': Artifact(Path)
+        })
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign({
+            'output_lammps': Artifact(Path, sub_path=False)
+        })
+
+    @OP.exec_sign_check
+    def execute(self, op_in: OPIO) -> OPIO:
+        cwd = os.getcwd()
+        os.chdir(op_in["input_lammps"])
+        cmd = "for ii in task.*; do cd $ii; lmp -in in.lammps; cd ..; done"
+        subprocess.call(cmd, shell=True)
+        os.chdir(cwd)
+        op_out = OPIO({
+            "output_lammps": op_in["input_lammps"]
+        })
+        return op_out
+
+
+class PropsPostLAMMPS(OP):
+    """
+    class for analyzing calculation results
+    """
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign({
+            'input_post': Artifact(Path, sub_path=False),
+            'path': str,
+            'input_all': Artifact(Path, sub_path=False)
+        })
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign({
+            'output_all': Artifact(Path, sub_path=False)
+        })
+
+    @OP.exec_sign_check
+    def execute(self, op_in: OPIO) -> OPIO:
+        os.chdir(str(op_in['input_all'])+op_in['path'])
+        shutil.copytree(str(op_in['input_post']) + op_in['path'], '../scripts/', dirs_exist_ok=True)
+
+        param_argv = 'param_prop.json'
+        cmd = f'dpgen autotest post {param_argv}'
+        subprocess.call(cmd, shell=True)
+
+        op_out = OPIO({
+            'output_all': Path(str(op_in["input_all"])+op_in['path'])
+        })
+        return op_out
---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dflow-props",
+    version="0.0.1",
+    author="Zhuoyuan Li",
+    author_email="zhuoyli@outlook.com",
+    description="A properties test package based on dflow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pydflow>=1.6.27",
+        "lbg>=1.2.13",
+        "dpdata>=0.2.13",
+        "matplotlib",
+        "dpgen",
+        "seekpath"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    provides=["dflowprops"],
+    script=[],
+    entry_point = {
+        'console_scripts': [
+            'dflowprops = scripts.__main__:main'
+        ]
+    }
+)
