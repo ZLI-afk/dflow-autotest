7f6aed113850af9b8b5c971c369c18ded145cb79
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2023-04-02 19:57:57 +0800

    change __init__ way

--- setup.py
+++ setup.py
@@ -15,10 +15,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "pydflow>=1.6.27",
-        "pymatgen>=2022.11.1",
         "lbg>=1.2.13",
         "dpdata>=0.2.13",
-        "ase",
         "matplotlib",
         "seekpath",
         "fpop"
