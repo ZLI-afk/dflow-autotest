f4430ead94e22df972b58ad04976567f2ce9ea7e
Zhuoyuan <75076820+ZLI-afk@users.noreply.github.com>
2023-04-21 20:06:29 +0800

    change name to apex

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dflow-autotest",
+    name="APEX",
     version="0.1.0",
     author="Zhuoyuan Li, Tongqi Wen",
     author_email="zhuoyli@outlook.com",
-    description="DPGEN autotest package based on dflow",
+    description="Alloy Properties EXplorer using simulations",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ZLI-afk/dflow-autotest.git",
+    url="https://github.com/deepmodeling/APEX.git",
     packages=setuptools.find_packages(),
     install_requires=[
         "pydflow>=1.6.27",
@@ -32,6 +32,6 @@ setuptools.setup(
     python_requires='>=3.8',
     script=[],
     entry_points={'console_scripts': [
-         'dflowautotest = dflowautotest.__main__:main',
+         'apex = apex.__main__:main',
      ]}
 )
